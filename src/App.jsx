95fbd356c814d5f0f7763ba2bc79543bb82f0928
Mohit Yadav <my701319@gmail.com>
2023-11-17 11:45:36 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/home/home";
+import Home from "./pages/home/home.jsx";
 
 function App() {
   return (
