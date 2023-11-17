7714fc8ab4af632b94abd62d7963d111cdf1533b
Mohit Yadav <my701319@gmail.com>
2023-11-17 18:32:32 +0530

    thirteenth commit

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,14 +5,8 @@ function Home() {
     <>
       <div className="flex flex-col w-full h-[100dvh] text-gray-300">
         <Header />
-        <div className="w-full h-[30%] bg-[#323232]"></div>
+        <div className="w-full h-[25%] bg-[#323232]"></div>
         <div className="w-full grow flex flex-col bg-[#212121] justify-around p-2">
-          <div className="w-full flex justify-around">
-            <div className="bg-[#323232] p-5 rounded-full text-4xl">AC</div>
-            <div className="bg-[#323232] p-5 rounded-full text-4xl">AC</div>
-            <div className="bg-[#323232] p-5 rounded-full text-4xl">AC</div>
-            <div className="bg-[#323232] p-5 rounded-full text-4xl">AC</div>
-          </div>
           <div className="w-full flex justify-around">
             <div className="bg-[#323232] p-5 rounded-full text-4xl">AC</div>
             <div className="bg-[#323232] p-5 rounded-full text-4xl">AC</div>
