1771f8d271e9e18fb5354fee7823b29830f5ca7e
Mohit Yadav <my701319@gmail.com>
2023-11-17 17:54:21 +0530

    tenth commit removing auth0

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,6 @@
 import ReactDOM from "react-dom/client";
-import { Auth0Provider } from "@auth0/auth0-react";
-import process from "process";
 
 import App from "./App.jsx";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <Auth0Provider
-    domain="dev-6frrjw7a16aew1yu.us.auth0.com"
-    clientId="fhoi0Pa21adE7gk4ODtbrzDCmfZ7Zb4n"
-    authorizationParams={{
-      redirect_uri: window.location.origin,
-    }}
-  >
-    <App />
-  </Auth0Provider>
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
