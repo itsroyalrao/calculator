29a7bac73efcaf38902648740f6a1e8b2f2a1a46
Mohit Yadav <my701319@gmail.com>
2023-11-17 15:14:15 +0530

    eighth commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,14 @@
 import ReactDOM from "react-dom/client";
 import { Auth0Provider } from "@auth0/auth0-react";
+import process from "process";
 
 import App from "./App.jsx";
 import "./index.css";
-import { getConfig } from "../config.js";
-
-const config = getConfig();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Auth0Provider
-    domain={config.domain}
-    clientId={config.clientId}
+    domain={process.env.DOMAIN}
+    clientId={process.env.CLIENT_ID}
     authorizationParams={{
       redirect_uri: window.location.origin,
     }}
