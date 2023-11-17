de4acb55aec7c22acd49660ce1a840cd7dbda8c4
Mohit Yadav <my701319@gmail.com>
2023-11-17 18:22:26 +0530

    eleventh commit

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,10 +3,40 @@ import Header from "../../components/Header";
 function Home() {
   return (
     <>
-      <div className="flex flex-col h-[100dvh] bg-[#242424] text-gray-300">
+      <div className="flex flex-col w-full h-[100dvh] text-gray-300">
         <Header />
-        <div className="grow  flex justify-center items-center text-4xl">
-          Har Har Mahadev
+        <div className="w-full h-[25%] bg-[#323232]"></div>
+        <div className="w-full grow flex flex-col bg-[#212121] justify-around">
+          <div className="w-full flex justify-around">
+            <div className="bg-[#323232] p-3 rounded-full text-3xl">AC</div>
+            <div className="bg-[#323232] p-3 rounded-full text-3xl">AC</div>
+            <div className="bg-[#323232] p-3 rounded-full text-3xl">AC</div>
+            <div className="bg-[#323232] p-3 rounded-full text-3xl">AC</div>
+          </div>
+          <div className="w-full flex justify-around">
+            <div className="bg-[#323232] p-3 rounded-full text-3xl">AC</div>
+            <div className="bg-[#323232] p-3 rounded-full text-3xl">AC</div>
+            <div className="bg-[#323232] p-3 rounded-full text-3xl">AC</div>
+            <div className="bg-[#323232] p-3 rounded-full text-3xl">AC</div>
+          </div>
+          <div className="w-full flex justify-around">
+            <div className="bg-[#323232] p-3 rounded-full text-3xl">AC</div>
+            <div className="bg-[#323232] p-3 rounded-full text-3xl">AC</div>
+            <div className="bg-[#323232] p-3 rounded-full text-3xl">AC</div>
+            <div className="bg-[#323232] p-3 rounded-full text-3xl">AC</div>
+          </div>
+          <div className="w-full flex justify-around">
+            <div className="bg-[#323232] p-3 rounded-full text-3xl">AC</div>
+            <div className="bg-[#323232] p-3 rounded-full text-3xl">AC</div>
+            <div className="bg-[#323232] p-3 rounded-full text-3xl">AC</div>
+            <div className="bg-[#323232] p-3 rounded-full text-3xl">AC</div>
+          </div>
+          <div className="w-full flex justify-around">
+            <div className="bg-[#323232] p-3 rounded-full text-3xl">AC</div>
+            <div className="bg-[#323232] p-3 rounded-full text-3xl">AC</div>
+            <div className="bg-[#323232] p-3 rounded-full text-3xl">AC</div>
+            <div className="bg-[#323232] p-3 rounded-full text-3xl">AC</div>
+          </div>
         </div>
       </div>
     </>
