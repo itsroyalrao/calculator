4d02d4e7492fdd33828fdac4f17a6a83f7c59b74
Mohit Yadav <my701319@gmail.com>
2023-11-18 00:28:32 +0530

    twenty-third commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 function Header() {
   return (
     <>
-      <div className="font-bold bg-[#202020] p-4 sm:px-4 sm:py-2 flex justify-between items-center tracking-wide">
+      <div className="font-bold bg-[#202020] p-4 tracking-wide">
         <div className="text-3xl">Calculator</div>
       </div>
     </>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,15 +2,40 @@ import { useState } from "react";
 import Header from "../../components/Header";
 
 function Home() {
-  const [value, setValue] = useState();
+  const [firstValue, setFirstValue] = useState(null);
+  const [total, setTotal] = useState(0);
+  const [expression, setExpression] = useState(null);
+
+  function arithmetic() {
+    if (!total) setTotal(total + firstValue);
+    else if (expression === "/") setTotal(total / firstValue);
+    else if (expression === "-") setTotal(total - firstValue);
+    else if (expression === "*") setTotal(total * firstValue);
+    else setTotal(total + firstValue);
+  }
 
   return (
     <>
       <div className="flex flex-col w-full h-[100dvh] text-gray-300">
         <Header />
-        <div className="w-full h-[25%] bg-[#323232]"></div>
+        <div className="w-full h-[25%] bg-[#323232]">
+          <div className="flex justify-end items-center h-full text-7xl pr-2 sm:pr-4">
+            {firstValue && total
+              ? total + expression + firstValue
+              : firstValue
+              ? firstValue
+              : total + expression}
+          </div>
+        </div>
         <div className="w-full grow grid grid-cols-4 gap-3 bg-[#212121] font-bold p-3 text-3xl sm:text-2xl">
-          <div className="flex justify-center items-center bg-red-600 rounded-full sm:cursor-pointer active:rounded-xl sm:hover:bg-red-500">
+          <div
+            className="flex justify-center items-center bg-red-600 rounded-full sm:cursor-pointer active:rounded-xl sm:hover:bg-red-500"
+            onClick={() => {
+              setFirstValue(null);
+              setExpression(null);
+              setTotal(0);
+            }}
+          >
             AC
           </div>
           <div className="flex justify-center items-center bg-[#323232] rounded-full sm:cursor-pointer active:rounded-xl sm:hover:bg-[#484848]">
@@ -19,55 +44,170 @@ function Home() {
           <div className="flex justify-center items-center bg-[#323232] rounded-full sm:cursor-pointer active:rounded-xl sm:hover:bg-[#484848]">
             %
           </div>
-          <div className="flex justify-center items-center bg-[#323232] rounded-full sm:cursor-pointer active:rounded-xl sm:hover:bg-[#484848]">
+          <div
+            className="flex justify-center items-center bg-[#323232] rounded-full sm:cursor-pointer active:rounded-xl sm:hover:bg-[#484848]"
+            onClick={() => {
+              setFirstValue(null);
+              setExpression("/");
+              arithmetic();
+            }}
+          >
             ÷
           </div>
-          <div className="flex justify-center items-center bg-[#323232] rounded-full sm:cursor-pointer active:rounded-xl sm:hover:bg-[#484848]">
+          <div
+            className="flex justify-center items-center bg-[#323232] rounded-full sm:cursor-pointer active:rounded-xl sm:hover:bg-[#484848]"
+            onClick={(e) => {
+              if (!firstValue) setFirstValue(parseInt(e.target.innerHTML));
+              else
+                setFirstValue(firstValue * 10 + parseInt(e.target.innerHTML));
+            }}
+          >
             7
           </div>
-          <div className="flex justify-center items-center bg-[#323232] rounded-full sm:cursor-pointer active:rounded-xl sm:hover:bg-[#484848]">
+          <div
+            className="flex justify-center items-center bg-[#323232] rounded-full sm:cursor-pointer active:rounded-xl sm:hover:bg-[#484848]"
+            onClick={(e) => {
+              if (!firstValue) setFirstValue(parseInt(e.target.innerHTML));
+              else
+                setFirstValue(firstValue * 10 + parseInt(e.target.innerHTML));
+            }}
+          >
             8
           </div>
-          <div className="flex justify-center items-center bg-[#323232] rounded-full sm:cursor-pointer active:rounded-xl sm:hover:bg-[#484848]">
+          <div
+            className="flex justify-center items-center bg-[#323232] rounded-full sm:cursor-pointer active:rounded-xl sm:hover:bg-[#484848]"
+            onClick={(e) => {
+              if (!firstValue) setFirstValue(parseInt(e.target.innerHTML));
+              else
+                setFirstValue(firstValue * 10 + parseInt(e.target.innerHTML));
+            }}
+          >
             9
           </div>
-          <div className="flex justify-center items-center bg-[#323232] rounded-full sm:cursor-pointer active:rounded-xl sm:hover:bg-[#484848]">
+          <div
+            className="flex justify-center items-center bg-[#323232] rounded-full sm:cursor-pointer active:rounded-xl sm:hover:bg-[#484848]"
+            onClick={() => {
+              setFirstValue(null);
+              setExpression("*");
+              arithmetic();
+            }}
+          >
             x
           </div>
-          <div className="flex justify-center items-center bg-[#323232] rounded-full sm:cursor-pointer active:rounded-xl sm:hover:bg-[#484848]">
+          <div
+            className="flex justify-center items-center bg-[#323232] rounded-full sm:cursor-pointer active:rounded-xl sm:hover:bg-[#484848]"
+            onClick={(e) => {
+              if (!firstValue) setFirstValue(parseInt(e.target.innerHTML));
+              else
+                setFirstValue(firstValue * 10 + parseInt(e.target.innerHTML));
+            }}
+          >
             4
           </div>
-          <div className="flex justify-center items-center bg-[#323232] rounded-full sm:cursor-pointer active:rounded-xl sm:hover:bg-[#484848]">
+          <div
+            className="flex justify-center items-center bg-[#323232] rounded-full sm:cursor-pointer active:rounded-xl sm:hover:bg-[#484848]"
+            onClick={(e) => {
+              if (!firstValue) setFirstValue(parseInt(e.target.innerHTML));
+              else
+                setFirstValue(firstValue * 10 + parseInt(e.target.innerHTML));
+            }}
+          >
             5
           </div>
-          <div className="flex justify-center items-center bg-[#323232] rounded-full sm:cursor-pointer active:rounded-xl sm:hover:bg-[#484848]">
+          <div
+            className="flex justify-center items-center bg-[#323232] rounded-full sm:cursor-pointer active:rounded-xl sm:hover:bg-[#484848]"
+            onClick={(e) => {
+              if (!firstValue) setFirstValue(parseInt(e.target.innerHTML));
+              else
+                setFirstValue(firstValue * 10 + parseInt(e.target.innerHTML));
+            }}
+          >
             6
           </div>
-          <div className="flex justify-center items-center bg-[#323232] rounded-full sm:cursor-pointer active:rounded-xl sm:hover:bg-[#484848]">
+          <div
+            className="flex justify-center items-center bg-[#323232] rounded-full sm:cursor-pointer active:rounded-xl sm:hover:bg-[#484848]"
+            onClick={() => {
+              setFirstValue(null);
+              setExpression("-");
+              arithmetic();
+            }}
+          >
             −
           </div>
-          <div className="flex justify-center items-center bg-[#323232] rounded-full sm:cursor-pointer active:rounded-xl sm:hover:bg-[#484848]">
+          <div
+            className="flex justify-center items-center bg-[#323232] rounded-full sm:cursor-pointer active:rounded-xl sm:hover:bg-[#484848]"
+            onClick={(e) => {
+              if (!firstValue) setFirstValue(parseInt(e.target.innerHTML));
+              else
+                setFirstValue(firstValue * 10 + parseInt(e.target.innerHTML));
+            }}
+          >
             1
           </div>
-          <div className="flex justify-center items-center bg-[#323232] rounded-full sm:cursor-pointer active:rounded-xl sm:hover:bg-[#484848]">
+          <div
+            className="flex justify-center items-center bg-[#323232] rounded-full sm:cursor-pointer active:rounded-xl sm:hover:bg-[#484848]"
+            onClick={(e) => {
+              if (!firstValue) setFirstValue(parseInt(e.target.innerHTML));
+              else
+                setFirstValue(firstValue * 10 + parseInt(e.target.innerHTML));
+            }}
+          >
             2
           </div>
-          <div className="flex justify-center items-center bg-[#323232] rounded-full sm:cursor-pointer active:rounded-xl sm:hover:bg-[#484848]">
+          <div
+            className="flex justify-center items-center bg-[#323232] rounded-full sm:cursor-pointer active:rounded-xl sm:hover:bg-[#484848]"
+            onClick={(e) => {
+              if (!firstValue) setFirstValue(parseInt(e.target.innerHTML));
+              else
+                setFirstValue(firstValue * 10 + parseInt(e.target.innerHTML));
+            }}
+          >
             3
           </div>
-          <div className="flex justify-center items-center bg-[#323232] rounded-full sm:cursor-pointer active:rounded-xl sm:hover:bg-[#484848]">
+          <div
+            className="flex justify-center items-center bg-[#323232] rounded-full sm:cursor-pointer active:rounded-xl sm:hover:bg-[#484848]"
+            onClick={() => {
+              setFirstValue(null);
+              setExpression("+");
+              arithmetic();
+            }}
+          >
             +
           </div>
-          <div className="flex justify-center items-center bg-[#323232] rounded-full sm:cursor-pointer active:rounded-xl sm:hover:bg-[#484848]">
+          <div
+            className="flex justify-center items-center bg-[#323232] rounded-full sm:cursor-pointer active:rounded-xl sm:hover:bg-[#484848]"
+            onClick={(e) => {
+              if (!firstValue) setFirstValue(parseInt(e.target.innerHTML));
+              else
+                setFirstValue(firstValue * 10 + parseInt(e.target.innerHTML));
+            }}
+          >
             0
           </div>
           <div className="flex justify-center items-center bg-[#323232] rounded-full sm:cursor-pointer active:rounded-xl sm:hover:bg-[#484848]">
             .
           </div>
-          <div className="flex justify-center items-center bg-[#323232] rounded-full sm:cursor-pointer active:rounded-xl sm:hover:bg-[#484848]">
+          <div
+            className="flex justify-center items-center bg-[#323232] rounded-full sm:cursor-pointer active:rounded-xl sm:hover:bg-[#484848]"
+            onClick={() => {
+              if (!(firstValue || expression)) setTotal(0);
+              else if (!firstValue) {
+                setFirstValue(total);
+                setTotal(0);
+                setExpression(null);
+              } else setFirstValue(Math.floor(firstValue / 10));
+            }}
+          >
             <img src="icons/backbtn.png" alt="b" className="w-5" />
           </div>
-          <div className="flex justify-center items-center bg-[#323232] rounded-full sm:cursor-pointer active:rounded-xl sm:hover:bg-[#484848]">
+          <div
+            className="flex justify-center items-center bg-[#323232] rounded-full sm:cursor-pointer active:rounded-xl sm:hover:bg-[#484848]"
+            onClick={() => {
+              setExpression(null);
+              setFirstValue(null);
+              arithmetic();
+            }}
+          >
             =
           </div>
         </div>
