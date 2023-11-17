3569ba151715917d59e8ecce5ff3f50b4d1983b8
Mohit Yadav <my701319@gmail.com>
2023-11-17 12:10:22 +0530

    fourth commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./components/pages/home/Home.jsx";
+import Home from "./pages/home/Home";
 
 function App() {
   return (
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,4 @@
-import Header from "../../Header";
+import Header from "../../components/Header";
 
 function Home() {
   return (
@@ -6,7 +6,7 @@ function Home() {
       <div className="flex flex-col h-[100dvh] bg-[#242424] text-gray-300">
         <Header />
         <div className="grow  flex justify-center items-center text-4xl">
-          Mahadev
+          Har Har Mahadev
         </div>
       </div>
     </>
